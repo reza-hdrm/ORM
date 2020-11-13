b01a9b0d08d5c3a9756db6c966e694682ee49f48
Reza Heydari Mehr <rezacolonelup@gmail.com>
2020-11-13 07:39:04 +0330

    SessionTest Developed

--- src/test/java/orm/SessionTest.java
+++ src/test/java/orm/SessionTest.java
@@ -8,17 +8,21 @@ import static org.junit.Assert.*;
 
 public class SessionTest {
     Session session;
+
     @Test
     public void save() {
-        session=new Session();
-        User user=new User();
+        session = new Session();
+        session.beginTransaction();
+        User user = new User();
         user.setFirstName("Reza");
         user.setLastName("Heydarimehr");
         user.setEmail("rezacolonelup@gmail.com");
         user.setMobile(933991484);
         session.save(user);
-        User userDB=(User)session.get(User.class,1);
-        Assert.assertEquals(" ",userDB.getFirstName(),"Reza");
+        User userDB = (User) session.get(User.class, 1);
+        session.commite();
+        session.close();
+        Assert.assertEquals(" ", userDB.getFirstName(), "Reza");
     }
 
     @Test
@@ -27,13 +31,25 @@ public class SessionTest {
 
     @Test
     public void get() {
-        session=new Session();
-        User userDB=(User)session.get(User.class,1);
-        Assert.assertEquals(" ",userDB.getFirstName(),"Reza");
+        session = new Session();
+        session.beginTransaction();
+        User userDB = (User) session.get(User.class, 3);
+        Assert.assertEquals(" ", userDB.getFirstName(), "Reza");
+        session.commite();
+        session.close();
     }
 
     @Test
     public void delete() {
+        //todo complete
+        session = new Session();
+        session.beginTransaction();
+        User userDB = (User) session.get(User.class, 3);
+        Assert.assertEquals(" ", userDB.getFirstName(), "Reza");
+        session.delete(userDB);
+        session.commite();
+        session.close();
+        Assert.assertNull(" ", userDB.getFirstName());
     }
 
     @Test
